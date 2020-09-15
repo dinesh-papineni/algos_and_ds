89f62ce976b507555ac35c5a6e3811057a96e277
Dinesh Papineni <dinesh.papineni@gmail.com>
2020-09-14 23:03:53 -0500

    address duplicates values in quick sort

--- quick_sort_recursion.py
+++ quick_sort_recursion.py
@@ -3,17 +3,19 @@ def quick_sort_recursion(arr):
     if (len(arr) < 2):
         return arr
     else:
-        # choose pivot at middle of array to avoid worst case run time
-        low = 0
-        high = len(arr) - 1
-        pivot = arr[(low + high) // 2]
-        # Can also choose first element as pivot
-        # pivot = arr[0]
-        smaller = [i for i in arr if i < pivot]
-        larger = [i for i in arr if i > pivot]
-        return quick_sort_recursion(smaller) + [pivot] + quick_sort_recursion(larger)
+        pivot = arr[0]
+        smaller, larger = [], []
+        for i in arr[1:]:
+            if i <= pivot:
+                smaller.append(i)
+            else:
+                larger.append(i)
+        return quick_sort_recursion(smaller) + [
+            pivot
+        ] + quick_sort_recursion(larger)
 
 
-arr = [-10, -5, 1, 2, 3, 4, 5]
+arr = [-10, -5, 1, 2, 3, -10, 4, 5]
 sorted_arr = quick_sort_recursion(arr)
 print(sorted_arr)
+
